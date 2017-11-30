74e9f4488d3306eb243a10ade7fe346241b20c3e
XhmikosR <xhmikosr@gmail.com>
2017-11-30 08:22:04 +0200

    cli.js: remove unneeded function.

--- cli.js
+++ cli.js
@@ -5,39 +5,37 @@
 const path = require('path');
 const fusv = require('./index');
 
-let globalSuccess = true;
+// The first and second args are: path/to/node script.js
+const args = process.argv.slice(2);
 
-function main(args) {
-    if (args.length < 1) {
-        console.log('Wrong arguments!');
-        console.log('Usage: find-unused-sass-variables folder [, folder2...]');
-        process.exit(1);
-    }
+let success = true;
 
-    args.forEach((arg) => {
-        const dir = path.resolve(arg);
-
-        console.log(`Finding unused variables in "${dir}"...`);
-
-        const unusedVars = fusv.find(dir);
-
-        console.log(`There's a total of ${unusedVars.total} variables.`);
-
-        unusedVars.unused.forEach((unusedVar) => {
-            console.log(`Variable "${unusedVar}" is only used once!`);
-        });
-
-        if (unusedVars.unused.length > 0) {
-            globalSuccess = false;
-        } else {
-            console.log('No unused variables found!');
-        }
-    });
-
-    if (globalSuccess === false) {
-        process.exit(1);
-    }
+if (args.length < 1) {
+    console.log('Wrong arguments!');
+    console.log('Usage: find-unused-sass-variables folder [, folder2...]');
+    process.exit(1);
 }
 
-// The first and second args are: path/to/node script.js
-main(process.argv.slice(2));
+args.forEach((arg) => {
+    const dir = path.resolve(arg);
+
+    console.log(`Finding unused variables in "${dir}"...`);
+
+    const unusedVars = fusv.find(dir);
+
+    console.log(`There's a total of ${unusedVars.total} variables.`);
+
+    unusedVars.unused.forEach((unusedVar) => {
+        console.log(`Variable "${unusedVar}" is only used once!`);
+    });
+
+    if (unusedVars.unused.length > 0) {
+        success = false;
+    } else {
+        console.log('No unused variables found!');
+    }
+});
+
+if (success === false) {
+    process.exit(1);
+}
