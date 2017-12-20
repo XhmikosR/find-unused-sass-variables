8f32dac83f293a7c4f2ece493d8df5edb38b1b4a
XhmikosR <xhmikosr@gmail.com>
2017-12-20 10:14:23 +0200

    Minor variable refactoring.

--- cli.js
+++ cli.js
@@ -3,17 +3,14 @@
 'use strict';
 
 const path = require('path');
-const fusv = require('./index.js');
 const chalk = require('chalk');
+const fusv = require('./index.js');
 
-// The first and second args are: path/to/node script.js
-const args = process.argv.slice(2);
-
+const args = process.argv.slice(2); // The first and second args are: path/to/node script.js
+const log = console.log;
 let success = true;
 
-const log = console.log;
-
-// colors
+// Colors
 const errorClr = chalk.red.bold;
 const infoClr = chalk.cyan;
 const successClr = chalk.green;
