270b30a5ebcdf9fafc0f7dbbf254e41cf3c68885
Johann-S <johann.servoire@gmail.com>
2017-12-22 10:17:37 +0100

    Use Ora

--- cli.js
+++ cli.js
@@ -4,6 +4,7 @@
 
 const path = require('path');
 const chalk = require('chalk');
+const ora = require('ora');
 const fusv = require('./index.js');
 
 const args = process.argv.slice(2); // The first and second args are: path/to/node script.js
@@ -13,7 +14,7 @@ let success = true;
 // Colors
 const errorClr = chalk.red.bold;
 const infoClr = chalk.cyan;
-const successClr = chalk.green;
+const reset = chalk.default;
 
 if (args.length < 1) {
     log(errorClr('Wrong arguments!'));
@@ -21,25 +22,30 @@ if (args.length < 1) {
     process.exit(1);
 }
 
+log('Looking for unused variables');
+const spinner = ora('').start();
+
 args.forEach((arg) => {
     const dir = path.resolve(arg);
 
-    log(`Finding unused variables in "${infoClr.bold(dir)}"...`);
+    spinner.info(`Finding unused variables in "${infoClr.bold(dir)}"...`);
 
     const unusedVars = fusv.find(dir);
 
-    log(`Found ${infoClr.bold(unusedVars.total)} total variables.`);
+    spinner.info(`${infoClr.bold(unusedVars.total)} total variables.`);
 
     unusedVars.unused.forEach((unusedVar) => {
-        log(chalk`{yellow Variable {reset.bold %s} is not being used!}`, unusedVar);
+        spinner.warn(chalk.yellow(`Variable ${reset.bold(unusedVar)} is not being used!`));
     });
 
     if (unusedVars.unused.length > 0) {
         success = false;
     } else {
-        log(successClr('No unused variables found!'));
+        spinner.succeed('No unused variables found!');
     }
 });
+
+spinner.stop();
 
 if (success === false) {
     process.exit(1);
