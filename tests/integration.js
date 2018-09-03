d756844c1b0d0c6796baf9da0e523cc5f340a3f1
Johann-S <johann.servoire@gmail.com>
2018-09-03 20:47:07 +0200

    add integration tests

---
+++ tests/integration.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const fusv = require('../index');
+
+const expectedUnused = [
+    '$white'
+];
+
+console.log('Run integration tests...');
+
+const result = fusv.find('./');
+
+if (result.total === expectedUnused.length) {
+    console.info(`All tests passed (${result.total})`);
+    process.exit(0);
+} else {
+    console.error(`Expect ${expectedUnused.length} and got ${result.total}`);
+    process.exit(1);
+}
