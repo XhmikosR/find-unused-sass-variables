37b6151c01b828be984b944ee396f7edfe1c601b
XhmikosR <xhmikosr@gmail.com>
2019-01-06 11:40:32 +0200

    Lint tweaks.

--- cli.js
+++ cli.js
@@ -5,20 +5,20 @@
 const path = require('path');
 const chalk = require('chalk');
 const ora = require('ora');
-const fusv = require('./index.js');
+const fusv = require('.');
 
 // The first and second args are: path/to/node script.js
 // If an argument starts with --, exclude the argument and the next argument.
 const args = process.argv.slice(2)
-                .filter((arg, i, list) => !arg.startsWith('--') && (i === 0 || !list[i - 1].startsWith('--')));
+    .filter((arg, i, list) => !arg.startsWith('--') && (i === 0 || !list[i - 1].startsWith('--')));
 
 // Ignored variables, comma separated.
 const ignore = process.argv.slice(2)
-                .filter((arg, i, list) => i !== 0 && list[i - 1] === '--ignore')
-                .join(',')
-                .split(',');
+    .filter((arg, i, list) => i !== 0 && list[i - 1] === '--ignore')
+    .join(',')
+    .split(',');
 
-const log = console.log;
+const { log } = console;
 let success = true;
 
 // Colors
@@ -26,7 +26,7 @@ const errorClr = chalk.red.bold;
 const infoClr = chalk.cyan;
 const reset = chalk.default;
 
-if (args.length < 1) {
+if (args.length === 0) {
     log(errorClr('Wrong arguments!'));
     log('Usage: find-unused-sass-variables folder [, folder2...]');
     process.exit(1);
@@ -35,7 +35,7 @@ if (args.length < 1) {
 log('Looking for unused variables');
 const spinner = ora('').start();
 
-args.forEach((arg) => {
+args.forEach(arg => {
     const dir = path.resolve(arg);
 
     spinner.info(`Finding unused variables in "${infoClr.bold(dir)}"...`);
@@ -44,7 +44,7 @@ args.forEach((arg) => {
 
     spinner.info(`${infoClr.bold(unusedVars.total)} total variables.`);
 
-    unusedVars.unused.forEach((unusedVar) => {
+    unusedVars.unused.forEach(unusedVar => {
         spinner.warn(chalk.yellow(`Variable ${reset.bold(unusedVar)} is not being used!`));
     });
 
