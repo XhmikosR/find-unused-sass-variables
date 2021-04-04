0814a9bb538160dab0ad3656665937453071dcdd
GeoSot <geo.sotis@gmail.com>
2021-04-05 02:30:09 +0300

    Add sync API method
    Rename sync function to `find` and async to `findAsync` to keep compatibility

--- tests/integration.js
+++ tests/integration.js
@@ -16,21 +16,32 @@ const expectedUnused = [
 
 const ignore = ['$ignored-variable'];
 
+const runTests = (type, result) => {
+    console.log(`Running ${type} integration tests...`);
+
+    if (result.unused.length === expectedUnused.length) {
+        console.log('Tests passed!');
+    } else {
+        throw new Error(
+            `Expected ${expectedUnused.length} unused variables and got ${result.unused.length}.\n` +
+            `Expected: ${expectedUnused.join(', ')}\n` +
+            `Got: ${result.unused.join(', ')}`
+        );
+    }
+};
+
+try {
+    const result = fusv.find('./tests/', { ignore });
+    runTests('Sync', result);
+} catch (error) {
+    console.error(error);
+    process.exit(1);
+}
+
 (async() => {
     try {
-        console.log('Running integration tests...');
-
-        const result = await fusv.find('./tests/', { ignore });
-
-        if (result.unused.length === expectedUnused.length) {
-            console.log('Tests passed!');
-        } else {
-            throw new Error(
-                `Expected ${expectedUnused.length} unused variables and got ${result.unused.length}.\n` +
-                `Expected: ${expectedUnused.join(', ')}\n` +
-                `Got: ${result.unused.join(', ')}`
-            );
-        }
+        const result = await fusv.findAsync('./tests/', { ignore });
+        runTests('async', result);
     } catch (error) {
         console.error(error);
         process.exit(1);
